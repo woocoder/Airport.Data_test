d7cce5391ed05ce15ebfa1bba523eaff5e28eb86
li <li@WOO-PC>
2016-09-05 13:51:53 +0800

    类型读取提交

---
+++ Airport.Data_test/DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Airport.Data_test
+{
+    class FlightInfo
+    {
+        public
+        string flightCode;
+        string flightType;
+        string d_or_i;
+        string startTime;
+        string endTime;
+        string isTurnArround;
+    }
+    class StandInfo
+    {
+        string standCode;
+        string near_or_far;
+        string priority;
+    }
+    class Prerules
+    {
+        List<Dictionary<FlightInfo,StandInfo>> finishTask;
+        List<Dictionary<StandInfo, StandInfo>> sCAdjCon;
+    }
+}
--- Airport.Data_test/Program.cs
+++ Airport.Data_test/Program.cs
@@ -3,13 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Airport.Gate.Data.Dao;
+using System.Data;
+using System.Data.OracleClient;
+using System.Configuration;
+
+
 
 namespace Airport.Data_test
 {
     class Program
     {
+        List<FlightInfo> flightList(string starttimeBegin, string starttimeEnd, string endtimeBegin, string endtimeEnd)
+        {
+            List<FlightInfo> flightList;
+            try
+            {
+                OrclDBManager orclDB_in = new OrclDBManager("gateDatain");
+                string flightSql = "select * from V_FLIGHT where STARTTIME >= :starttimeBegin and STARTTIME <= :starttimeEnd";
+                Dictionary<string, object> paramDic = new Dictionary<string, object>();
+                paramDic.Add("starttimeBegin", Convert.ToDateTime("2015/08/13 19:20:00"));
+                paramDic.Add("starttimeEnd", Convert.ToDateTime("2015/08/13 22:20:00"));
+                DataTable a = orclDB_in.GetDataTable(flightSql, paramDic);
+
+                for (int i = 0; i != a.Rows; i++)
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+
+                Console.Read();
+            }
+            return flightList;
+        }
         static void Main(string[] args)
         {
+            
+          
+            
+
         }
     }
 }
